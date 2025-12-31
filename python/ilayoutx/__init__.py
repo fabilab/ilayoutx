faa272e6a57b75c71efbf6668bf9f9c6e99b1927
Fabio Zanini <fabio.zanini@fastmail.fm>
2026-01-01 09:47:00 +1100

    Try to make CI happy with various submodule import tricks

--- python/ilayoutx/__init__.py
+++ python/ilayoutx/__init__.py
@@ -1,5 +1,7 @@
 """ilayoutx root module."""
 
-import ilayoutx.layouts as layouts  # noqa: F401
-import ilayoutx.packing as packing  # noqa: F401
-import ilayoutx.routing as routing  # noqa: F401
+from ilayoutx import (
+    layouts,
+    packing,
+    routing,
+)
